3381c4e7c18473cbfac7bad645a4dc16d8af78aa
Martin Lettry <m.lettry1@gmail.com>
2023-11-22 09:42:43 +0100

    milestone ex2: potential solution
     * asked MDR for review

--- assign7.py
+++ assign7.py
@@ -15,11 +15,26 @@ def compute_midpoint():
             y -= 1
             f += 2 * dx
         f -= 2 * dy
-compute_midpoint()
-# (3,8)
-# (4,8)
-# (5,7)
-# (6,7)
-# (7,6)
-# (8,6)
-# (9,5)
+
+
+def rasterize_triangle(p1, p2, p3):
+    # Sort vertices in counter-clockwise order
+    vertices = sort_vertices(p1, p2, p3)
+
+    # Divide mesh into two subtriangles
+    subtriangles = divide_mesh(vertices)
+
+    for triangle in subtriangles:
+        # Construct bounding box for the subtriangle
+        bounding_box = calculate_bounding_box(triangle)
+
+        # Iterate pixels from left to right, bottom to top
+        for x in range(bounding_box.left, bounding_box.right + 1):
+            for y in range(bounding_box.bottom, bounding_box.top + 1):
+                # Calculate barycentric coordinates
+                barycentric_coords = calculate_barycentric_coords(triangle, x, y)
+
+                # Check if the pixel center lies inside the triangle
+                if is_inside_triangle(barycentric_coords):
+                    # Color the pixel
+                    color_pixel(x, y)
